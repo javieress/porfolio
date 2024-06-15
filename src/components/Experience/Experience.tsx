8cb8f7fa142a32eff41ca461a29cdf2e1349ce1e
Javier Rojas <64340938+javieress@users.noreply.github.com>
2024-06-15 13:49:35 -0400

    feat: Add Experience component and styles

---
+++ src/components/Experience/Experience.tsx
@@ -0,0 +1,39 @@
+import ExperienceItem from "./ExperienceItem";
+
+import "../../styles/Experience/Experience.css";
+
+const experiences = [
+  {
+    title: "Desarrollador Web",
+    company: "Empresa",
+    date: "2020 - 2021",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget urna et nulla consectetur.",
+  },
+  {
+    title: "Desarrollador Web",
+    company: "Empresa",
+    date: "2020 - 2021",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget urna et nulla consectetur.",
+  },
+];
+
+export default function Experience() {
+  return (
+    <section className="experience">
+      {/* title */}
+      <h1 className="experience_title">Experiencia</h1>
+      {/* experience items */}
+      {experiences.map((experience, index) => (
+        <ExperienceItem
+          key={index}
+          title={experience.title}
+          company={experience.company}
+          date={experience.date}
+          description={experience.description}
+        />
+      ))}
+    </section>
+  );
+}
---
+++ src/components/Experience/ExperienceItem.tsx
@@ -0,0 +1,32 @@
+import "../../styles/Experience/ExperienceItem.css";
+
+type ExperienceItemProps = {
+  title: string;
+  company: string;
+  date: string;
+  description: string;
+};
+
+export default function ExperienceItem({
+  title,
+  company,
+  date,
+  description,
+}: ExperienceItemProps) {
+  return (
+    <section className="experience_item">
+      <section className="experience_item_left">
+        {/* title */}
+        <h2 className="experience_item_title">{title}</h2>
+        {/* company */}
+        <p className="experience_item_company">{company}</p>
+        {/* date */}
+        <p className="experience_item_date">{date}</p>
+      </section>
+      <section className="experience_item_right">
+        {/* description */}
+        <p className="experience_item_description">{description}</p>
+      </section>
+    </section>
+  );
+}

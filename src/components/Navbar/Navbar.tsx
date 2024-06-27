46fba1ff27bc601a25314a0d538fea4799215be9
Javier Rojas <64340938+javieress@users.noreply.github.com>
2024-06-26 22:13:58 -0400

    refactor: Update Navbar and css for less to 640 screens

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -15,30 +15,24 @@ import Image from "next/image";
 // importar svg logo 'alma.svg'
 
 export default function PortfolioNavbar() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
   const menuItems = [
     {
       title: "Proyectos",
-      url: "/#projects",
+      url: "projects",
     },
     {
       title: "Experiencia",
-      url: "/#experience",
+      url: "experience",
     },
     {
       title: "Sobre mi",
-      url: "/#about-me",
+      url: "about-me",
     },
   ];
 
   return (
-    <Navbar onMenuOpenChange={setIsMenuOpen}>
+    <Navbar shouldHideOnScroll>
       <NavbarContent>
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="sm:hidden"
-        />
         <NavbarBrand>
           <a href="#" className="flex items-center gap-2">
             <Image src="/alma.svg" alt="Javier Rojas" width={40} height={40} />
@@ -50,7 +44,7 @@ export default function PortfolioNavbar() {
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         {menuItems.map((item, index) => (
           <NavbarItem key={index}>
-            <Link href={item.url}>{item.title}</Link>
+            <Link href={`/#${item.url}`}>{item.title}</Link>
           </NavbarItem>
         ))}
       </NavbarContent>
@@ -65,13 +59,44 @@ export default function PortfolioNavbar() {
           </Link>
         </NavbarItem>
       </NavbarContent>
-      <NavbarMenu>
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <Link href={item.url}>{item.title}</Link>
-          </NavbarMenuItem>
-        ))}
-      </NavbarMenu>
     </Navbar>
   );
 }
+
+// export default function App() {
+//   return (
+//     <Navbar shouldHideOnScroll>
+//       <NavbarBrand>
+//         <AcmeLogo />
+//         <p className="font-bold text-inherit">ACME</p>
+//       </NavbarBrand>
+//       <NavbarContent className="hidden sm:flex gap-4" justify="center">
+//         <NavbarItem>
+//           <Link color="foreground" href="#">
+//             Features
+//           </Link>
+//         </NavbarItem>
+//         <NavbarItem isActive>
+//           <Link href="#" aria-current="page">
+//             Customers
+//           </Link>
+//         </NavbarItem>
+//         <NavbarItem>
+//           <Link color="foreground" href="#">
+//             Integrations
+//           </Link>
+//         </NavbarItem>
+//       </NavbarContent>
+//       <NavbarContent justify="end">
+//         <NavbarItem className="hidden lg:flex">
+//           <Link href="#">Login</Link>
+//         </NavbarItem>
+//         <NavbarItem>
+//           <Button as={Link} color="primary" href="#" variant="flat">
+//             Sign Up
+//           </Button>
+//         </NavbarItem>
+//       </NavbarContent>
+//     </Navbar>
+//   );
+// }

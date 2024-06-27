aab1dcd4eddf3286dc27f9a174a82d54a72b78eb
Javier Rojas <64340938+javieress@users.noreply.github.com>
2024-06-26 22:33:04 -0400

    refactor: Update Navbar and css for less to 640 screens

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -31,7 +31,7 @@ export default function PortfolioNavbar() {
   ];
 
   return (
-    <Navbar shouldHideOnScroll>
+    <Navbar>
       <NavbarContent>
         <NavbarBrand>
           <a href="#" className="flex items-center gap-2">
@@ -62,41 +62,3 @@ export default function PortfolioNavbar() {
     </Navbar>
   );
 }
-
-// export default function App() {
-//   return (
-//     <Navbar shouldHideOnScroll>
-//       <NavbarBrand>
-//         <AcmeLogo />
-//         <p className="font-bold text-inherit">ACME</p>
-//       </NavbarBrand>
-//       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-//         <NavbarItem>
-//           <Link color="foreground" href="#">
-//             Features
-//           </Link>
-//         </NavbarItem>
-//         <NavbarItem isActive>
-//           <Link href="#" aria-current="page">
-//             Customers
-//           </Link>
-//         </NavbarItem>
-//         <NavbarItem>
-//           <Link color="foreground" href="#">
-//             Integrations
-//           </Link>
-//         </NavbarItem>
-//       </NavbarContent>
-//       <NavbarContent justify="end">
-//         <NavbarItem className="hidden lg:flex">
-//           <Link href="#">Login</Link>
-//         </NavbarItem>
-//         <NavbarItem>
-//           <Button as={Link} color="primary" href="#" variant="flat">
-//             Sign Up
-//           </Button>
-//         </NavbarItem>
-//       </NavbarContent>
-//     </Navbar>
-//   );
-// }
